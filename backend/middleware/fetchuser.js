2cb6440d8e9f1feb8e5bf6cc1f084d074e108665
sandip kukadiya <160720138+imsendyyy@users.noreply.github.com>
2024-05-02 10:56:31 +0530

    Update fetchuser.js
    in console.log(data) we get id not user so data.user will be undifind when this function runs in Rout it will throws error like - cant read pro. undefind , 
     and we alse have to chnge in auth funcion 
    from ( userId = req.user.id ) to ( userId = req.id )

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -9,7 +9,8 @@ const fetchuser = (req, res, next) => {
     }
     try {
         const data = jwt.verify(token, JWT_SECRET);
-        req.user = data.user;
+        // in console.log(data) we get id not user so data.user will be undifind when this function runs in Rout it will throws error cant read pro. undefind
+        req.id = data.id;
         next();
     } catch (error) {
         res.status(401).send({ error: "Please authenticate using a valid token" })
